18d85810373780cdc7bf17d0a24417ab85589572
juliet-wambua <julienea289@gmail.com>
2019-10-06 22:26:49 +0300

    created splash_animation.xml to store my images for the splash screen activity

--- app/src/main/java/com/nea/myemo/SplashScreenActivity.java
+++ app/src/main/java/com/nea/myemo/SplashScreenActivity.java
@@ -1,4 +1,46 @@
 package com.nea.myemo;
 
-public class SplashScreenActivity {
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 6000;
+    private ImageView container;
+    private AnimationDrawable animationDrawable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        container = findViewById(R.id.iv_icons);
+        container.setBackgroundResource(R.drawable.splash_animation);
+        animationDrawable = (AnimationDrawable) container.getBackground();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        animationDrawable.start();
+        checkAnimationStatus(150, animationDrawable);
+    }
+    /**
+     * check the animation status recursively, keep the animation until it reach the last frame.
+     *
+     * @param time              period of animation
+     * @param animationDrawable animation list
+     */
+    private void checkAnimationStatus(final int time, final AnimationDrawable animationDrawable) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (animationDrawable.getCurrent() != animationDrawable.getFrame(animationDrawable.getNumberOfFrames() - 1))
+                    checkAnimationStatus(time, animationDrawable);
+                else finish();
+            }
+        }, time);
+    }
 }
