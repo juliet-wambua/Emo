258b47579e610888d15755595b2d3e9ef0c3784a
juliet-wambua <julienea289@gmail.com>
2019-10-07 08:15:55 +0300

    implemeted butterknife

--- app/src/main/java/com/nea/myemo/MainActivity.java
+++ app/src/main/java/com/nea/myemo/MainActivity.java
@@ -20,5 +20,7 @@ import android.widget.TextView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+        ButterKnife.bind ( this );
+
     }
 }
